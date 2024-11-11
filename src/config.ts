7cb1b0281c50caa4cfb543dd327b1f1d5a3d4b28
Wardi <wardi@nusa.net.id>
2024-11-11 11:08:59 +0700

    refactor: remove MySQL environment variables and config references
    - Removed MySQL-related environment variables from `.env.dist`.
    - Deleted MySQL configuration from `src/config.ts`.

--- src/config.ts
+++ src/config.ts
@@ -3,11 +3,6 @@ import { config } from 'dotenv'
 config()
 
 export const PORT = process.env.PORT || 3000
-export const MYSQL_HOST = process.env.MYSQL_HOST || 'localhost'
-export const MYSQL_USER = process.env.MYSQL_USER || 'root'
-export const MYSQL_PORT = process.env.MYSQL_PORT || 3306
-export const MYSQL_PASSWORD = process.env.MYSQL_PASSWORD || 'supersecret'
-export const MYSQL_DATABASE = process.env.MYSQL_DATABASE || 'test'
 export const DEFAULT_SESSION = process.env.DEFAULT_SESSION || 'main'
 export const LOG_DIR = process.env.LOG_DIR || '/tmp'
 export const MEDIA_DIR = process.env.MEDIA_DIR || '/tmp'
